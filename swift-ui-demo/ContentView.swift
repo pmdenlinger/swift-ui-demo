6ac91a2b79f18b92cff5d344f90a40841b88f464
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-08 19:51:36 -0700

    Add state variable to store text string, text field, then style text field with modifier.

--- swift-ui-demo/ContentView.swift
+++ swift-ui-demo/ContentView.swift
@@ -10,15 +10,22 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var rotation: Double = 0
+    @State private var text: String = "Welcome to SwiftUI"
+    
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .rotationEffect(.degrees(rotation))
-                .animation(.easeInOut)
-            Slider(value: $rotation, in: 0...360, step: 0.1)
+            VStack {
+                Text(text)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .rotationEffect(.degrees(rotation))
+                    .animation(.easeInOut)
+                Slider(value: $rotation, in: 0...360, step: 0.1)
+                TextField("Enter text here", text: $text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                 
+
+            }
         }
     }
 }
