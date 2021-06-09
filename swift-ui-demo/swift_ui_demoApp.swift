7292f6fac408a1a879688480ff849fbe8b3ed354
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-08 19:15:45 -0700

    Initial Commit

---
+++ swift-ui-demo/swift_ui_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_demoApp.swift
+//  swift-ui-demo
+//
+//  Created by user on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
