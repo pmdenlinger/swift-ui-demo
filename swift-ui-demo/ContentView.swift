2f6dd475af13c976bb4581a6e4d40e4cf3b50046
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-08 20:12:55 -0700

    Add colorIndex state variable, colors, color names arrays, and picker with ForEach range to integrate through picker.

--- swift-ui-demo/ContentView.swift
+++ swift-ui-demo/ContentView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var colorIndex = 0
     @State private var rotation: Double = 0
     @State private var text: String = "Welcome to SwiftUI"
     
     var body: some View {
+        
+        let colors: [Color] = [.black, .red, .green, .blue]
+        let colornames = ["Black", "Red", "Green", "Blue"]
+        
         VStack {
             VStack {
                 Text(text)
@@ -20,10 +25,15 @@ struct ContentView: View {
                     .fontWeight(.heavy)
                     .rotationEffect(.degrees(rotation))
                     .animation(.easeInOut)
+                    .foregroundColor(colors[colorIndex])
                 Slider(value: $rotation, in: 0...360, step: 0.1)
                 TextField("Enter text here", text: $text)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                
+                Picker(selection: $colorIndex, label: Text("Color")) {
+                    ForEach (0 ..< colornames.count)  {
+                        Text(colornames[$0])
+                            .foregroundColor(colors[$0])
+                }
 
             }
         }
@@ -34,4 +44,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
